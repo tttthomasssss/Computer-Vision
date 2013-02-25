24ece987b5f3120e1feb33a7282cbff40ad08d83
tttthomasssss <th0mas.ko6er@gmail.com>
2013-02-25 21:59:54 +0000

    feature detector v1

--- feature_detection.m
+++ feature_detection.m
@@ -1,11 +1,15 @@
 function img = feature_detection(image)
     
     % Hardcoded for the chess1.bmp image
-    conv_mask = image(255:285, 286:322);
+    conv_mask = image(255:325, 286:322);
+    
+    %templ = flipud(conv_mask);
+    %templ = rot90(conv_mask);
+    %templ = rot90(templ);
+    templ = rot90(rot90(conv_mask));
     
     % Convolve
-    conv_img = convolve2(image, conv_mask, 'valid');
-    
+    conv_img = convolve2(image, templ, 'valid');
     img = normalise_matrix(conv_img, 1, 0);
     
     
